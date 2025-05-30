2ffd51f5ef0f154a50ea4a0682d3e95c9febae8d
ArturoLlb25 <112043493+ArturoLlb25@users.noreply.github.com>
2025-05-30 15:41:59 -0600

    Update

--- stoppage-time/src/environments/environment.ts
+++ stoppage-time/src/environments/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://v3.football.api-sports.io',
-  apiKey: '9f63c4bbf4881cd6893190661c9beb88',
-  backendUrl: 'http://localhost:3000/api'
-};
+
